8342945f51e76fa3b567e27041f0359b39f0ae93
silpa83 <154290742+silpa83@users.noreply.github.com>
2024-11-09 22:30:38 -0800

    Create stylescript.js

---
+++ stylescript.js
@@ -0,0 +1,9 @@
+// Toggle the dropdown menu on and off
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
