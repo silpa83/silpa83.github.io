985940670dcd3ebafd882dbfe93081460e49e0d2
silpa83 <154290742+silpa83@users.noreply.github.com>
2024-11-10 00:53:01 -0800

    Update stylescript.js

--- stylescript.js
+++ stylescript.js
@@ -1,11 +1,11 @@
 // Toggle the dropdown menu on and off
 document.getElementById('menu-icon').addEventListener('click', function() {
     console.log("Menu icon clicked"); // This should print to the console when you click the icon
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    if (dropdownMenu.style.display === 'block') {
-        dropdownMenu.style.display = 'none';
+    const navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
     } else {
-        dropdownMenu.style.display = 'block';
+        navLinks.style.display = 'flex';
     }
 });
 
